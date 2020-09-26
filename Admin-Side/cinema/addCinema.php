d419cb655cd413b2f3ab5c1099f6d66e0244d1c2
Nitsu <35673024+nitsuzxz@users.noreply.github.com>
2020-09-26 21:30:41 +0800

    CRUD
    finish CRUD function for Cinema

--- Admin-Side/cinema/addCinema.php
+++ Admin-Side/cinema/addCinema.php
@@ -1,5 +1,13 @@
 <?php
-	include ('../../Config/db_config.php');
+  include ('../../Config/db_config.php');
+
+  $query = 'SELECT * FROM cinema ORDER BY cinema_id ASC';
+
+  $res = mysqli_query($conn, $query);
+  $cinemaList = mysqli_fetch_all($res, MYSQLI_ASSOC);
+  $cinemaName = '';
+    $cinemaLocation = '';
+  $cinemaDesc = '';
 
 	if(isset($_POST['addCinema'])){
 		$cinemaName = $_POST['cinemaName'];
@@ -16,8 +24,40 @@
 		else{
 			echo 'ERROR'.mysqli_error($conn);
 
+
 		}
 	}
+  if (isset($_GET['delete'])) {
+    $query = "DELETE FROM cinema WHERE cinema_id = {$_GET['delete']}";
+    mysqli_query($conn,$query);
+    header('Location: cinema.php');
+  }
+
+  if (isset($_GET['edit'])){
+    $query = "SELECT * FROM cinema WHERE cinema_id = {$_GET['edit']}";
+    $res = mysqli_query($conn, $query);
+    $cinemaInst = mysqli_fetch_array($res);
+
+    $cinemaName = $cinemaInst['cinema_name'];
+    $cinemaLocation = $cinemaInst['cinema_location'];
+    $cinemaDesc = $cinemaInst['cinema_description'];
+  } 
+
+  if (isset($_POST['editCinema'])){
+    $cinemaID= $_POST['cinID']; 
+    $cinemaName = $_POST['cinemaName'];
+    $cinemaLocation = $_POST['cinemaLocation'];
+    $cinemaDesc = $_POST['cinemaDesc'];
+
+	$query = "UPDATE cinema SET cinema_name = '$cinemaName', cinema_location='$cinemaLocation', cinema_description='$cinemaDesc' WHERE cinema_id = '$cinemaID'";
+	 mysqli_query($conn, $query);
+	 header('Location: cinema.php');
+  }
+  // Free Result
+  mysqli_free_result($res);
+
+  // Close Connection
+  mysqli_close($conn);
 
 
 ?>
