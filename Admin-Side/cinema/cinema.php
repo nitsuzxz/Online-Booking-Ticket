20064b037bf1a07df00c4cdddb2492745b0adac4
Nitsu <35673024+nitsuzxz@users.noreply.github.com>
2020-09-26 21:30:07 +0800

    Update cinema.php
    remove query to addcinema.php

--- Admin-Side/cinema/cinema.php
+++ Admin-Side/cinema/cinema.php
@@ -1,42 +1,5 @@
 <?php
-  include ('../../Config/db_config.php');
-
-  $query = 'SELECT * FROM cinema ORDER BY cinema_id ASC';
-
-  $res = mysqli_query($conn, $query);
-  $cinemaList = mysqli_fetch_all($res, MYSQLI_ASSOC);
-  $cinemaName = '';
-    $cinemaLocation = '';
-  $cinemaDesc = '';
-  if (isset($_GET['delete'])) {
-    $query = "DELETE FROM cinema WHERE cinema_id = {$_GET['delete']}";
-    mysqli_query($conn,$query);
-    header('Location: cinema.php');
-  }
-
-  if (isset($_GET['edit'])){
-    $query = "SELECT * FROM cinema WHERE cinema_id = {$_GET['edit']}";
-    $res = mysqli_query($conn, $query);
-    $cinemaInst = mysqli_fetch_array($res);
-
-    $cinemaName = $cinemaInst['cinema_name'];
-    $cinemaLocation = $cinemaInst['cinema_location'];
-    $cinemaDesc = $cinemaInst['cinema_description'];
-  } 
-
-  if (isset($_POST['editCinema'])){
-    echo $_POST['cinemaName'];
-    $cinemaName = $_POST['cinemaName'];
-    $cinemaLocation = $_POST['cinemaLocation'];
-    $cinemaDesc = $_POST['cinemaDesc'];
-
-    //$query = "UPDATE cinema SET cinema_name = $cinemaName, cinema_location = $cinemaLocation, cinema_description = $cinemaDesc WHERE cinema_id = ";
-  }
-  // Free Result
-  mysqli_free_result($res);
-
-  // Close Connection
-  mysqli_close($conn);
+include './addCinema.php'
 ?>
 
 <!DOCTYPE html>
@@ -60,7 +23,7 @@
       <div id="main">
           <h2>Manage Cinema</h2>
 
-          <form class="col-8" action="./addCinema.php" method="post" style="margin: 0px 20px;">
+          <form class="col-8" action="" method="post" style="margin: 0px 20px;">
             <div class="form-group col-8">
               <label for="inputName">Name</label>
               <input type="text" class="form-control col-12" id="inputName" name="cinemaName" placeholder="Enter Name" value="<?php echo $cinemaName ?>" required>
