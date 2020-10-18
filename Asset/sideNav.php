dfe28c742f9a212e428294df6eb0381348abc3df
MFaiz21 <69975959+MFaiz21@users.noreply.github.com>
2020-10-18 16:17:01 +0800

    Manage Staff SideNav

--- Asset/sideNav.php
+++ Asset/sideNav.php
@@ -12,6 +12,7 @@
 			<a href="../movie/movie.php">Manage Movies</a>
 			<a href="../hall/hall.php">Manage Halls</a>
 			<a href="../cinema/cinema.php">Manage Cinemas</a>
+			<a href="../staff/staff.php">Manage Staff</a>
 		</div>
 	</body>
 
