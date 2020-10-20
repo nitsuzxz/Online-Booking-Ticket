92cdc572821f328ac74ef39164d31e275553f9c4
Nitsu <35673024+nitsuzxz@users.noreply.github.com>
2020-10-20 17:58:19 +0800

    Update seat.php
    fetch showtime details

--- Client-Side/Seat/seat.php
+++ Client-Side/Seat/seat.php
@@ -2,16 +2,22 @@
 
 include "../../Config/db_config.php";
 
-//variable to filter the result...
-// $cinema_id=''; 
-// $movie_id=''; 
-// $hall='';
-// $time='';
-// $data='';
+if(isset($_POST['showtimeInfo'])){
 
-// $movie_id=mysqli_real_escape_string($conn, $movie_id);
+    $aired_id=$_POST['showtimeInfo'];
+    //get movie name, date, showtime
+    $query="SELECT aired.aired_date,aired.aired_startTime,movie.movie_name,hall.hall_name,cinema.cinema_name FROM aired
+     INNER JOIN movie on aired.movie_id=movie.movie_id
+     INNER JOIN hall on aired.hall_id= hall.hall_id
+     INNER JOIN cinema on hall.hall_cinema_id= cinema.cinema_id
+     WHERE aired.aired_id=".$aired_id."";
 
-// $fetch
+    $res=mysqli_query($conn,$query);
+    $fetch_showtimeDetail=mysqli_fetch_all($res,MYSQLI_ASSOC);
+    
+    print json_encode($fetch_showtimeDetail);
+
+}
 
 if(isset($_POST['airedID'])){
 
