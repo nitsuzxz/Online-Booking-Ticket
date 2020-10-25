c4fe072e20b3489b58622e86f5e5ddf5a0264b69
Nitsu <35673024+nitsuzxz@users.noreply.github.com>
2020-10-25 18:02:38 +0800

    Update selectMovie.php
    fetch showtime

--- Client-Side/MainPage/selectMovie.php
+++ Client-Side/MainPage/selectMovie.php
@@ -29,15 +29,16 @@ if(isset($_POST["getMovie_currentDate"])){
     
             if($status=='upcoming'){
 
-                $query_updateStatus= "UPDATE movie SET movie_status='aired' WHERE movie_id= $movie_id";
+                $query_updateStatus= "UPDATE movie SET movie_status='airing' WHERE movie_id= $movie_id";
                 $run=mysqli_query($conn,$query_updateStatus);
             
             }
         }
     }
 
+
     
-    $query="SELECT * FROM movie WHERE movie_status='aired'";
+    $query="SELECT * FROM movie WHERE movie_status='airing'";
     $run=mysqli_query($conn,$query);
     $fetch=mysqli_fetch_all($run,MYSQLI_ASSOC);
 
@@ -46,16 +47,18 @@ if(isset($_POST["getMovie_currentDate"])){
 }
 
 
-if(isset($_POST['sel_movie']) && $_POST['currentDate']){
+if(isset($_POST["sel_movie"], $_POST["currentDate"])){
 
 
     $movie_id= $_POST['sel_movie'];
     $currentDate= $_POST['currentDate'];
+    
 
-    $query="SELECT aired_startTime,aired.aired_date,aired.movie_id, hall.hall_name, cinema.cinema_name FROM aired 
-            INNER JOIN hall ON aired.hall_id=hall.hall_id 
-            INNER JOIN cinema on hall.hall_cinema_id=cinema.cinema_id
-            WHERE aired.movie_id=$movie_id AND aired.aired_date>='$currentDate'";
+    $query="SELECT aired_date FROM aired WHERE movie_id=$movie_id AND aired_date>=$currentDate GROUP BY aired_date ";
+    // $query="SELECT aired_startTime,aired.aired_date,aired.movie_id, hall.hall_name, cinema.cinema_name FROM aired 
+    //         INNER JOIN hall ON aired.hall_id=hall.hall_id 
+    //         INNER JOIN cinema on hall.hall_cinema_id=cinema.cinema_id
+    //         WHERE aired.movie_id=$movie_id AND aired.aired_date>='$currentDate'";
 
     $run=mysqli_query($conn,$query);
     $fetch=mysqli_fetch_all($run,MYSQLI_ASSOC);
@@ -64,6 +67,25 @@ if(isset($_POST['sel_movie']) && $_POST['currentDate']){
     print json_encode($fetch);
 
     }
+
+if(isset($_POST["m_id"] , $_POST["m_date"])){
+
+    $movie_id= $_POST ["m_id"];
+    $movie_airedDate= $_POST["m_date"];
+
+        $query="SELECT aired.aired_id,aired.aired_startTime,aired.aired_date,aired.movie_id, hall.hall_name, cinema.cinema_name FROM aired 
+            INNER JOIN hall ON aired.hall_id=hall.hall_id 
+            INNER JOIN cinema on hall.hall_cinema_id=cinema.cinema_id
+            WHERE aired.movie_id=$movie_id AND aired.aired_date='$movie_airedDate'";
+
+        $run=mysqli_query($conn,$query);
+        $fetch=mysqli_fetch_all($run,MYSQLI_ASSOC);
+
+        print json_encode($fetch);
+
+        
+
+}
 
 
 ?>
