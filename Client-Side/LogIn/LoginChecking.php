b4d98340fc436126d0d4339de9571cd6f497960b
Nitsu <35673024+nitsuzxz@users.noreply.github.com>
2020-10-25 18:02:13 +0800

    Update LoginChecking.php
    add condition to check either user has select showtime or not

--- Client-Side/LogIn/LoginChecking.php
+++ Client-Side/LogIn/LoginChecking.php
@@ -3,7 +3,7 @@
         $email =$_POST['email'];
         $password =$_POST['password'];
 
-        $checking ="SELECT  user_email, user_password FROM user WHERE user_email = '$email' AND user_password = '$password'";
+        $checking ="SELECT user_id,user_email, user_password FROM user WHERE user_email = '$email' AND user_password = '$password'";
         $check =mysqli_query($conn, $checking ) or die(mysqli_error($conn)); // line nie untuk run instruction from $checking
         $row = $check->fetch_assoc();// cara get data
 
@@ -20,9 +20,24 @@
 
             ?>
                 <script>
-                   window.location.href="../MainPage/MainPage.html";
+                    var aired=JSON.parse(sessionStorage.getItem("airedID"));
+                    var user_id=JSON.parse(sessionStorage.getItem("user_id"));
+
+
+               
+                    sessionStorage.setItem("user_id", JSON.stringify(<?php echo $row['user_id']; ?>));
+
+
+                    if(aired==null){
+                          window.location.href="../index.html";
+                     }else{
+                         window.location.href = "../Seat/seat.html";
+                     }
+
                 </script>
                 <?php
 
         }
 ?>
+
+
