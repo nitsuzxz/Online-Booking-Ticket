af18215146861b2d626fcd6324a221bb6e98241f
Nitsu <35673024+nitsuzxz@users.noreply.github.com>
2020-10-27 22:14:47 +0800

    Update selectMovie.php

--- Client-Side/MainPage/selectMovie.php
+++ Client-Side/MainPage/selectMovie.php
@@ -5,22 +5,28 @@ if(isset($_POST["getMovie_currentDate"])){
 
     $currentDate=$_POST["getMovie_currentDate"]; 
 
-    $query="SELECT * FROM movie WHERE movie_status='aired' OR movie_status='upcoming'";
+    $query="SELECT * FROM movie WHERE movie_status='airing' OR movie_status='upcoming'";
     $run=mysqli_query($conn,$query);
     $fetch=mysqli_fetch_all($run,MYSQLI_ASSOC);
-    
+
+
     foreach($fetch as $moviedetails){
 
         $movie_id   = $moviedetails['movie_id'];
         $startDate  = $moviedetails['movie_date_start_aired'];
         $endDate    = $moviedetails['movie_date_end_aired'];
         $status     = $moviedetails['movie_status'];
+    
+
 
         if($currentDate>$endDate){
 
-            if($status=="aired"){
+            echo "pass";
+
+            if($status=="airing"){
 
                 $query_updateStatus= "UPDATE movie SET movie_status='finished' WHERE movie_id= $movie_id";
+                echo $query_updateStatus;
                 $run=mysqli_query($conn,$query_updateStatus);
             
             }
@@ -30,14 +36,13 @@ if(isset($_POST["getMovie_currentDate"])){
             if($status=='upcoming'){
 
                 $query_updateStatus= "UPDATE movie SET movie_status='airing' WHERE movie_id= $movie_id";
+                echo $query_updateStatus;
                 $run=mysqli_query($conn,$query_updateStatus);
             
             }
         }
     }
 
-
-    
     $query="SELECT * FROM movie WHERE movie_status='airing'";
     $run=mysqli_query($conn,$query);
     $fetch=mysqli_fetch_all($run,MYSQLI_ASSOC);
@@ -54,12 +59,9 @@ if(isset($_POST["sel_movie"], $_POST["currentDate"])){
     $currentDate= $_POST['currentDate'];
     
 
-    $query="SELECT aired_date FROM aired WHERE movie_id=$movie_id AND aired_date>=$currentDate GROUP BY aired_date ";
-    // $query="SELECT aired_startTime,aired.aired_date,aired.movie_id, hall.hall_name, cinema.cinema_name FROM aired 
-    //         INNER JOIN hall ON aired.hall_id=hall.hall_id 
-    //         INNER JOIN cinema on hall.hall_cinema_id=cinema.cinema_id
-    //         WHERE aired.movie_id=$movie_id AND aired.aired_date>='$currentDate'";
-
+    $query="SELECT aired_date FROM aired WHERE movie_id=$movie_id AND aired_date>='$currentDate' GROUP BY aired_date";
+   
+    echo $query;
     $run=mysqli_query($conn,$query);
     $fetch=mysqli_fetch_all($run,MYSQLI_ASSOC);
 
